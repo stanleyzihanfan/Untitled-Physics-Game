c8243990d9816ca742335ffe67717981faffb923
Eyeseminal <148734630+stanleyzihanfan@users.noreply.github.com>
2026-01-09 13:37:31 -0800

    test

--- main.js
+++ main.js
@@ -42,4 +42,5 @@ const toggleLogBtn=document.getElementById("toggle-log");
 const logPanel=document.getElementById("log-panel");
 toggleLogBtn.addEventListener("click",()=>{
   logPanel.classList.toggle("hidden");
+  console.log("Clicked");
 });
